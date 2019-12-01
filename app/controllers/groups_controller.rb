753ef9ce8b1d33b2a947ccf59c0a799a42afea40
yuna-kusuhara <narlocked.222.b@gmail.com>
2019-12-01 13:29:22 +0900

    create_group-edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,9 @@
 class GroupsController < ApplicationController
-  
+  before_action :group_set, only: [:edit, :update]
+
+  def index
+  end
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -18,12 +22,21 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to root_path, notice: "グループを更新しました。"
+    else
+      render :edit
+    end
   end
   
   private
 
   def group_params
     params.require(:group).permit(:name, user_ids: [])
+  end
+
+  def group_set
+    @group = Group.find(params[:id])
   end
 
 end
